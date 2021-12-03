7a89866390c3816de1227f662ec867d2bf9a5c72
Lipika <72999725+lipikas@users.noreply.github.com>
2021-12-02 21:24:31 -0500

    added project files

---
+++ app/src/main/java/com/project5/DeluxePizza.java
@@ -0,0 +1,37 @@
+package com.project5;
+/**
+ * Creates delux pizza
+ * @author Lipika, Kenisha
+ */
+public class DeluxePizza extends Pizza{
+    private static final int NUM_FREE_TOPPINGS = 5;
+    private static final double BASE_PRICE = 12.99;
+
+    /**
+     * Constructor to create Deluxe Pizza object
+     */
+    public DeluxePizza(){
+        size = Size.Small;
+        toppings.add(Toppings.Sausage);
+        toppings.add(Toppings.GreenPepper);
+        toppings.add(Toppings.Onion);
+        toppings.add(Toppings.Pepperoni);
+        toppings.add(Toppings.Mushroom);
+    }
+
+    /**
+     * Calculates price for the pizza
+     * @return - price of the pizza
+     */
+    public double price(){
+        double price = BASE_PRICE;
+        int numOfToppings = toppings.size();
+        if(numOfToppings > NUM_FREE_TOPPINGS){
+            price += (numOfToppings - NUM_FREE_TOPPINGS) * FEE_PER_EXTRA_TOPPING;
+        }
+        if(size != Size.Small){
+            price += (size == Size.Medium) ? FEE_PER_SIZE_INCREASE : (2 * FEE_PER_SIZE_INCREASE);
+        }
+        return price;
+    }
+}
